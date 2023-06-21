85394157919c5137342e2ff56fea1f8839d44b6a
annikaferguson <fergusak@mail.uc.edu>
2023-06-21 13:57:25 -0400

    initial

---
+++ src/A3.java
@@ -0,0 +1,11 @@
+public class A3 {
+    public static void main(String[] args)
+    {
+
+        for(int r = 0; r <= 18; r + 3)
+        {
+            System.out.println("" + r);
+        }
+    }
+}
+
