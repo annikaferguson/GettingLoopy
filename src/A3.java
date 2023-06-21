adf2dfa0ca5fa6369603b11eed3154ac9b9ecbd1
annikaferguson <fergusak@mail.uc.edu>
2023-06-21 14:14:07 -0400

    final

--- src/A3.java
+++ src/A3.java
@@ -2,7 +2,7 @@ public class A3 {
     public static void main(String[] args)
     {
 
-        for(int r = 0; r <= 18; r + 3)
+        for(int r = 0; r <= 18; r += 3)
         {
             System.out.println("" + r);
         }
