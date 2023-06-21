6c6135a43ae7a47e60e56c89fe7a30612a41a350
annikaferguson <fergusak@mail.uc.edu>
2023-06-21 14:16:26 -0400

    final

---
+++ src/A4.java
@@ -0,0 +1,10 @@
+public class A4 {
+    public static void main(String[] args)
+    {
+
+        for(int r = 10; r >= 0; r -= 2)
+        {
+            System.out.println("" + r);
+        }
+    }
+}
