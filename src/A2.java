6954c1e90ad3e4837b07e1178890c5a225e3df15
annikaferguson <fergusak@mail.uc.edu>
2023-06-21 13:56:22 -0400

    final

--- src/A2.java
+++ src/A2.java
@@ -1,19 +1,13 @@
 import java.util.Scanner;
 
-public class A2
-{
+public class A2 {
     public static void main(String[] args)
     {
-        Scanner in = new Scanner(System.in);
-        String msg = "";
-        int numReps = 0;
-        String trash = "";
-        boolean done = false;
 
-        for(int r = 1; r <= 30; r --)
+        for(int r = 30; r >= 0; r --)
         {
             System.out.println("" + r);
         }
     }
 }
-}
+
