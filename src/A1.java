77c9da58e6e8e882c3bc5e235efe9d9f5c276a97
annikaferguson <fergusak@mail.uc.edu>
2023-06-21 13:50:53 -0400

    Initial commit

---
+++ src/A1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String msg = "";
+        int numReps = 0;
+        String trash = "";
+        boolean done = false;
+
+        for(int r = 1; r <= 30; r ++)
+        {
+            System.out.println("" + r);
+        }
+    }
+}
