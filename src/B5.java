c1beda5afbb16352d0a86d3d70bc822f856648e3
annikaferguson <fergusak@mail.uc.edu>
2023-06-21 19:50:00 -0400

    final

---
+++ src/B5.java
@@ -0,0 +1,33 @@
+public class B5 {
+    public static void main(String[] args)
+    {
+        for(int row = 1; row <=5; row ++)
+        {
+            for (int col = 1; col <= 5; col += 5)
+            {
+                System.out.printf("|");
+            }
+            System.out.println();
+            for (int col = 1; col <= 5; col += 4)
+            {
+                System.out.printf("|");
+            }
+            System.out.println();
+            for (int col = 1; col <= 5; col += 3)
+            {
+                System.out.printf("|");
+            }
+            System.out.println();
+            for (int col = 1; col <= 5; col += 2)
+            {
+                System.out.printf("|");
+            }
+            System.out.println();
+            for (int col = 1; col <= 5; col += 1)
+            {
+                System.out.printf("|");
+            }
+            System.out.println();
+        }
+    }
+}
